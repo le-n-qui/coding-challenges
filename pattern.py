f9e327ecb4460c713da3fef4504179375ff7d830
Qui Le <quil.eureka@gmail.com>
2019-03-26 19:47:53 -0700

    Add function header and decription of the problem

---
+++ pattern.py
@@ -0,0 +1,13 @@
+ # Pattern Printing
+ # Input: n (integer)
+ # Output: pattern
+ # If n = 3, we have 
+ #1*2*3*10*11*12
+ #--4*5*8*9
+ #----6*7
+
+ # Qui Le
+ # 03/26/2019
+ 
+ def pattern_printing(n):
+ 	pass
