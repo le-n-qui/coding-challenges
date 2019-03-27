9b26f6101d3375907e404b13747d40905306e588
Qui Le <qnle@ucdavis.edu>
2019-03-26 23:07:15 -0700

    Calculate the largest number in the pattern; Generate and store all numbers of pattern in list

--- pattern.py
+++ pattern.py
@@ -1,13 +1,20 @@
- # Pattern Printing
- # Input: n (integer)
- # Output: pattern
- # If n = 3, we have 
- #1*2*3*10*11*12
- #--4*5*8*9
- #----6*7
+# Pattern Printing
+# Input: n (integer)
+# Output: pattern
+# If n = 3, we have 
+#1*2*3*10*11*12
+#--4*5*8*9
+#----6*7
 
- # Qui Le
- # 03/26/2019
- 
- def pattern_printing(n):
- 	pass
+# Qui Le
+# 03/26/2019
+
+def pattern_printing(n):
+ 	# biggest number in the pattern
+ 	biggest_num = n * (n+1)
+
+ 	# generate all numbers in the pattern
+ 	# and hold them in the list
+ 	num_list = list(range(1, biggest_num)) 
+
+ 	
