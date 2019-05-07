f03bc2c8a6e92faadcf76c19e5443319076c8121
Qui Le <qnle@ucdavis.edu>
2019-05-06 19:05:20 -0700

    Provide Details on the Objective and What Script Output Will Look Like

--- directory_lookup.py
+++ directory_lookup.py
@@ -1,5 +1,15 @@
-# Second experiment with walk() from module os
-# Track progress with git 
+# Experiment with walk() from module os
 # Qui Le
 # May 6, 2019
+
+# Objective: Create a script to display information
+# within a directory given by a developer. This script
+# will systematically and recursively look inside
+# each subdirectory if there is any.
+
+# Below Is What We Can Expect to See
+# Name of Directory
+# List of Subdirectories
+# List of Files
+
 
