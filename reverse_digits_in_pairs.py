0b9885e77d29a64699f928fe0968aad04cd6ad43
Qui Le <qnle@ucdavis.edu>
2020-05-23 11:20:23 -0700

    Create function header and unit testing class for the problem as follows: reverse digits of a given integer in pairs

---
+++ reverse_digits_in_pairs.py
@@ -0,0 +1,22 @@
+# Coding Question
+# Qui Le
+
+import unittest
+
+# Problem Statement: Given an integer, reverse the digits of the integer in pairs.
+
+def reverse_digits_in_pairs(number):
+	pass
+
+
+# Unit Testing
+class TestReverseFn(unittest.TestCase):
+	def test_number_of_even_length(self):
+		pass
+
+	def test_number_of_odd_length(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
