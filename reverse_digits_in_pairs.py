4f1d44f81cbc2d4a3bd121a2235f69fe32006c37
Qui Le <qnle@ucdavis.edu>
2020-05-25 15:37:49 -0700

    Add comments on function implementation

--- reverse_digits_in_pairs.py
+++ reverse_digits_in_pairs.py
@@ -5,9 +5,18 @@ import unittest
 
 # Problem Statement: Given an integer, reverse the digits of the integer in pairs.
 
+# This function will use Python list as a stack
+# data structure to store digits of integer
 def reverse_digits_in_pairs(number):
+	# Need a stack
+	# Need a result variable
+	# With a while loop, get each digit of integer starting at the last one
+	# Keep digits in stack
+	# While stack is not empty, do two pop operations 
+	# Need a test condition for the second pop because stack may have only one item left
+	# Create reversed pairs of of digits and add it to result
+	# Return result
 	pass
-
 
 # Unit Testing
 class TestReverseFn(unittest.TestCase):
