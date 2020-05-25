8507601ef75e00bbd9d87ccb2f50b3e8403188ff
Qui Le <qnle@ucdavis.edu>
2020-05-25 16:07:05 -0700

    Add implementation code. Function passed two unit tests.

--- reverse_digits_in_pairs.py
+++ reverse_digits_in_pairs.py
@@ -9,14 +9,29 @@ import unittest
 # data structure to store digits of integer
 def reverse_digits_in_pairs(number):
 	# Need a stack
+	stack = []
 	# Need a result variable
+	result = 0
 	# With a while loop, get each digit of integer starting at the last one
-	# Keep digits in stack
+	while number:
+		# Keep digits in stack
+		stack.append(number % 10) # modulo 10 gives last digit
+		number //= 10 # Get the integer without last digit
 	# While stack is not empty, do two pop operations 
-	# Need a test condition for the second pop because stack may have only one item left
-	# Create reversed pairs of of digits and add it to result
+	while stack:
+		first_digit = stack.pop()
+		# Need a test condition for the second pop because stack may have only one item left
+		if stack:
+			second_digit = stack.pop()
+			# Create reversed pairs of of digits and add it to result
+			result = (result * 100) + (second_digit * 10) + first_digit
+		else:
+			# because second digit does not exist
+			# multiply result by 10 and add first digit
+			result = (result * 10) + first_digit
+		
 	# Return result
-	pass
+	return result
 
 # Unit Testing
 class TestReverseFn(unittest.TestCase):
