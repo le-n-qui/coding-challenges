04e1b2a938c0a010b54b707dfdebf4fbcb87ac56
Andy Le <andy.quile@gmail.com>
2021-07-22 22:00:51 -0700

    Outline main tasks

--- bioinfo/dna_nt_counter.py
+++ bioinfo/dna_nt_counter.py
@@ -4,3 +4,18 @@
 # Return: Four integers (separated by spaces) counting the 
 #   respective number of times that the symbols 'A', 'C', 
 #   'G', and 'T' occur in s.
+
+import unittest
+
+# Define function to count
+# nucleotides in a DNA string
+def dna_nt_counter(sample):
+	pass
+
+# Make a Unit Test class
+class TestStringDNA(unittest.TestCase):
+	pass
+
+# Verify tests by running this file on terminal
+if __name__ == '__main__':
+ 	unittest.main(verbosity=3) 
