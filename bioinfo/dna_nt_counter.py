7f5ce3a777e2cb04607a043d73c032dc4e40ec8b
Andy Le <andy.quile@gmail.com>
2021-07-22 21:41:35 -0700

    Create bioinfo folder and add the first Rosalind problem along with description

---
+++ bioinfo/dna_nt_counter.py
@@ -0,0 +1,6 @@
+# Rosalind: Problem 1
+# DNA string is a string constructed from the alphabet {A, C, G, T}
+# Given: A DNA string s of length at most 1000 nt (nucleotides).
+# Return: Four integers (separated by spaces) counting the 
+#   respective number of times that the symbols 'A', 'C', 
+#   'G', and 'T' occur in s.
