abd3816f87c5a0b852d08ca2039713acc2c61011
Andy Le <andy.quile@gmail.com>
2021-07-26 23:59:55 -0700

    Implement function

--- bioinfo/dna_nt_counter.py
+++ bioinfo/dna_nt_counter.py
@@ -10,7 +10,18 @@ import unittest
 # Define function to count
 # nucleotides in a DNA string
 def dna_nt_counter(sample):
-	pass
+	# create a counter dictionary to 
+	# keep count of each symbol
+	counter = dict()
+	for symbol in sample:
+		# if this is the first time 
+		# seeing the current symbol
+		if counter.get(symbol, -1) == -1:  
+			counter[symbol] = 1 # record 1
+		else:
+			# otherwise increment symbol's count by 1
+			counter[symbol] += 1
+	print(f"{counter['A']} {counter['C']} {counter['G']} {counter['T']}")
 
 # Make a Unit Test class
 class TestStringDNA(unittest.TestCase):
