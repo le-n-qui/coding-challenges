b559589540eaf59c1ee3a8f9d09fd9b839b3f49c
Andy Le <andy.quile@gmail.com>
2021-07-28 23:19:37 -0700

    Add comments and a plan of development

--- bioinfo/dna_nt_counter.py
+++ bioinfo/dna_nt_counter.py
@@ -1,11 +1,26 @@
-# Rosalind: Problem 1
-# DNA string is a string constructed from the alphabet {A, C, G, T}
-# Given: A DNA string s of length at most 1000 nt (nucleotides).
-# Return: Four integers (separated by spaces) counting the 
-#   respective number of times that the symbols 'A', 'C', 
-#   'G', and 'T' occur in s.
+# Bioinformatics Challenge Question
+# Programmer: Andy Qui
 
-import unittest
+"""
+Rosalind: Problem 1
+
+DNA string is a string constructed from the alphabet {A, C, G, T}
+
+Given: A DNA string s of length at most 1000 nt (nucleotides).
+Return: Four integers (separated by spaces) counting the 
+   respective number of times that the symbols 'A', 'C', 
+   'G', and 'T' occur in s.
+
+In comments and code, you may see two terms that are used 
+interchangeably and they are symbol and nucleotide.
+
+Development Plan:
+ - Create a function to count nucleotides in a sample DNA string
+ - Display count results for each symbol in our function
+ - Read files containing DNA strings on command line
+"""
+
+
 
 # Define function to count
 # nucleotides in a DNA string
